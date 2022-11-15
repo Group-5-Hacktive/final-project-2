0cfc2ac1eccd5a762830f56730a7bda43ba4266c
Muhammad Rafli Saputra <112410023+rafli0409@users.noreply.github.com>
2022-11-15 16:52:16 +0700

    update-navbar

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "300px";
+  // document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+
 // Date Time Card
 (function currentTime() {
   // buat object date lalu simpan di variable date

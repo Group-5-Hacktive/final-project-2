c5ffa79db65405034365dadbbfd5480b7643df4c
ahmad mukafi andrian <kafin20@gmail.com>
2022-11-16 16:16:42 +0700

    update bug feature 2

--- javascript/index.js
+++ javascript/index.js
@@ -1,25 +1,15 @@
 function formatAMPM(date) {
-    // ambil jam
+
     let hours = date.getHours();
-    // ambil menit
     let minutes = date.getMinutes();
-  
-    // tentukan am atau pm dengan check jam nya, jika jam nya lebih dari samadengan 12, maka return pm, sebaliknya return am
     let ampm = hours >= 12 ? "PM" : "AM";
   
-    // jam di modulo 12 untuk format 12 jam
     hours = hours % 12;
   
-    // di sini di check lagi, apakah dia bernilai 0? karena 0 itu false maka dia check nya hours ?
-    // ingat 0 bernilai false
-    hours = hours ? hours : 12; // the hour '0' should be '12'
-    // check jika jam  kuran dari 10, maka tambahkan 0 di depannya
+    hours = hours ? hours : 12; 
     hours = hours < 10 ? `0${hours}` : hours;
-  
-    // check jika minute kurang dari 10, maka tambahkan 0 di depannya
     minutes = minutes < 10 ? "0" + minutes : minutes;
   
-    // tampung di variable strTime
     let strTime = hours + ":" + minutes + " " + ampm;
     return strTime;
   }
@@ -28,27 +18,17 @@ function formatAMPM(date) {
  
 
   function formatAMPM(date) {
-    // ambil jam
+    
     let hours = date.getHours();
-    // ambil menit
     let minutes = date.getMinutes();
-  
-    // tentukan am atau pm dengan check jam nya, jika jam nya lebih dari samadengan 12, maka return pm, sebaliknya return am
     let ampm = hours >= 12 ? "PM" : "AM";
   
-    // jam di modulo 12 untuk format 12 jam
     hours = hours % 12;
   
-    // di sini di check lagi, apakah dia bernilai 0? karena 0 itu false maka dia check nya hours ?
-    // ingat 0 bernilai false
-    hours = hours ? hours : 12; // the hour '0' should be '12'
-    // check jika jam  kuran dari 10, maka tambahkan 0 di depannya
+    hours = hours ? hours : 12;
     hours = hours < 10 ? `0${hours}` : hours;
-  
-    // check jika minute kurang dari 10, maka tambahkan 0 di depannya
     minutes = minutes < 10 ? "0" + minutes : minutes;
   
-    // tampung di variable strTime
     let strTime = hours + ":" + minutes + " " + ampm;
     return strTime;
   }
@@ -56,29 +36,18 @@ function formatAMPM(date) {
   document.getElementById("realtime2").innerText = formatAMPM(new Date());
 
   function formatAMPM(date) {
-    // ambil jam
+    
     let hours = date.getHours();
-    // ambil menit
     let minutes = date.getMinutes();
-  
-    // tentukan am atau pm dengan check jam nya, jika jam nya lebih dari samadengan 12, maka return pm, sebaliknya return am
     let ampm = hours >= 12 ? "PM" : "AM";
   
-    // jam di modulo 12 untuk format 12 jam
     hours = hours % 12;
   
-    // di sini di check lagi, apakah dia bernilai 0? karena 0 itu false maka dia check nya hours ?
-    // ingat 0 bernilai false
-    hours = hours ? hours : 12; // the hour '0' should be '12'
-    // check jika jam  kuran dari 10, maka tambahkan 0 di depannya
+    hours = hours ? hours : 12;
     hours = hours < 10 ? `0${hours}` : hours;
-  
-    // check jika minute kurang dari 10, maka tambahkan 0 di depannya
     minutes = minutes < 10 ? "0" + minutes : minutes;
   
-    // tampung di variable strTime
     let strTime = hours + ":" + minutes + " " + ampm;
     return strTime;
   }
-
   document.getElementById("realtime3").innerText = formatAMPM(new Date());

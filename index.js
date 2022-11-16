8278752a25f6d330522fdd63ab55c94493fda2ed
Muhammad Rafli Saputra <112410023+rafli0409@users.noreply.github.com>
2022-11-16 15:27:40 +0700

    done section A

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ function openNav() {
   // document.getElementById("main").style.marginLeft = "250px";
 }
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
@@ -17,20 +16,20 @@ function closeNav() {
   let mm = date.getMinutes();
 
   // jika jam kurang dari 10, maka tambahkan 0 di depan jam ex: 05
-  hh = hh < 10 ? "0" + hh : hh;
+  hh = hh < 10 ? "0${hh}" : hh;
   // jika menit kurang dari 10, maka tambahkan 0 di depan menit ex: 05
   mm = mm < 10 ? "0" + mm : mm;
 
-  let getAmPm = hh >= 12 ? " PM" : " AM";
+  let getampm = hh >= 12 ? " PM" : " AM";
   if (hh >= 13) {
     hh -= 12;
   }
 
-  let time = hh + ":" + mm;
+  let time = hh + ":" + mm + " ";
 
-  document.getElementById("clock").innerText = time + getAmPm;
-  document.getElementById("clock-2").innerText = time + getAmPm;
-  document.getElementById("clock-3").innerText = time + getAmPm;
+  document.getElementById("clock").innerText = time + getampm;
+  document.getElementById("clock-2").innerText = time + getampm;
+  document.getElementById("clock-3").innerText = time + getampm;
   // jalankan function currentTime setiap 1000ms /1 detik
   let t = setTimeout(function () {
     currentTime();
